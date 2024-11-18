9f9ac3549ae8a3b0fd5988682f0402c9eb0d9856
GoldenMine0502 <ehe123@naver.com>
2024-11-19 07:47:42 +0900

    작동 확인. 아마도

--- extract_keywords.py
+++ extract_keywords.py
@@ -1,12 +1,8 @@
 from pathlib import Path
-
-from PyKomoran import Komoran
 from keybert import KeyBERT
-from kobert_tokenizer import KoBERTTokenizer
 from konlpy.tag import Okt
 from tqdm import tqdm
-from transformers import BertModel, BertTokenizer
-import torch
+from transformers import BertModel
 from collections import defaultdict
 
 
@@ -15,7 +11,6 @@ def list_all_files(directory):
 
 
 def get_stop_words():
-
     josa_list = [
         "이", "가", "을", "를", "의", "에", "에서", "으로", "로",
         "에게", "한테", "에게서", "한테서", "까지", "부터", "와", "과",
@@ -46,6 +41,7 @@ def get_stop_words():
 
     return stop_words
 
+
 def main():
     model_name = "skt/kobert-base-v1"
     # tokenizer = KoBERTTokenizer.from_pretrained(model_name)
@@ -60,7 +56,7 @@ def main():
     # 모든 문서에 대한 형태소 분석
     print("morphing...")
     all_morphs = []
-    for path in tqdm(list_all_files('speaker_transcriptions')[:2], ncols=75):
+    for path in tqdm(list_all_files('speaker_transcriptions'), ncols=75):
         with open(path, 'rt') as file:
             text = file.read()
         text = okt.morphs(text)
@@ -70,26 +66,42 @@ def main():
     stop_words = get_stop_words()
 
     # 키워드 추출
-    print("extracting keywords...")
-    all_keywords = []
-    for morphs in tqdm(all_morphs, ncols=75):
-        keywords = kw_model.extract_keywords(morphs, keyphrase_ngram_range=(1, 1), stop_words=stop_words, top_n=1000)
-        all_keywords.append(keywords)
+    def extract_keywords(ngram_range=(1, 1), output_file='result.txt'):
+        print(f"extracting keywords... {ngram_range} {output_file}")
+        all_keywords = []
+        for morphs in tqdm(all_morphs, ncols=75):
+            keywords = kw_model.extract_keywords(
+                morphs,
+                keyphrase_ngram_range=ngram_range,
+                stop_words=stop_words,
+                top_n=1000
+            )
+            all_keywords.append(keywords)
 
-    # 키워드 병합
-    merged_keywords = defaultdict(float)
-    for doc_keywords in all_keywords:
-        for keyword, score in doc_keywords:
-            merged_keywords[keyword] += score  # 중요도 합산
+        # 키워드 병합
+        merged_keywords = defaultdict(float)
+        for doc_keywords in all_keywords:
+            for keyword, score in doc_keywords:
+                merged_keywords[keyword] += score  # 중요도 합산
 
-    # 중요도 기준으로 정렬
-    sorted_keywords = sorted(merged_keywords.items(), key=lambda x: x[1], reverse=True)
-    keywords = list(map(lambda x: f'{x[0]}, {x[1]}', sorted_keywords))
-    # keywords = [text.replace(' ', '') for text in keywords]
+        # 중요도 기준으로 정렬
+        sorted_keywords = sorted(merged_keywords.items(), key=lambda x: x[1], reverse=True)
 
-    # result.txt 파일에 개행 단위로 저장
-    with open("result.txt", "wt", encoding="utf-8") as file:
-        file.write('\n'.join(keywords))  # 문자열 리스트를 개행 단위로 연결 후 파일에 쓰기
+        # 1만개로 제한
+        sorted_keywords = sorted_keywords[:10000]
+
+        # 키워드 문자열로 변환
+        keywords = list(map(lambda x: f'{x[0]}, {x[1]}', sorted_keywords))
+
+        # keywords = [text.replace(' ', '') for text in keywords]
+
+        # result.txt 파일에 개행 단위로 저장
+        with open(output_file, 'wt', encoding='utf-8') as file:
+            file.write('\n'.join(keywords))  # 문자열 리스트를 개행 단위로 연결 후 파일에 쓰기
+
+    extract_keywords((1, 1), 'result_11.txt')
+    extract_keywords((1, 2), 'result_12.txt')
+    extract_keywords((2, 2), 'result_22.txt')
 
 
 if __name__ == '__main__':
